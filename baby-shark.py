ffde9affbec62c384007262c068193179bbf9b32
jadedtdt <genebbb333@aol.com>
2019-02-20 10:37:50 -0500

    V2
    168 characters

--- baby-shark.py
+++ baby-shark.py
@@ -1,9 +1,4 @@
-s = ["Baby", "Mommy", "Daddy", "Grandma", "Grandpa", "Let's go hunt", "Run away", "Safe at last", "It's the end"]
-for i in range(0, 9):
-	b = s[i] + ' shark' if i < 5 else s[i]
-	for j in range(1, 4):
-		a = 'doo'
-		for k in range(1, 6):
-			a += str(' doo')
-		print b + ', ' + a
-	print b + '!'
+for w in"Baby,Mommy,Daddy,Grandma,Grandpa,Let's go hunt,Run away,Safe at last,It's the end".split(','):
+	if len(w)<8:
+		w+=" shark";
+	print(w+","+" doo"*6+"\n")*3+w+"!"
